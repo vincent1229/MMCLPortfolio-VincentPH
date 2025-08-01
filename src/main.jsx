8ade031df6736e8ead9e18fb895ad81ed9752f4e
vincent1229 <72140120+vincent1229@users.noreply.github.com>
2025-08-01 23:35:01 +0800

    code for now

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '@fontsource/inter/variable.css'
 import '@fontsource/manrope/variable.css'
-import './styles/theme.css'
+import './theme.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
