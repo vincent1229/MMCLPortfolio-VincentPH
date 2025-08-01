ea3e072fd2faba1a6f91897b17167dc80bd6e832
vincent1229 <72140120+vincent1229@users.noreply.github.com>
2025-08-01 15:59:22 +0000

    try

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,20 @@
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',
+    host: "0.0.0.0",
     port: 5173,
     strictPort: true,
     hmr: {
-      clientPort: 5173
+      clientPort: 5173,
     },
-    disableHostCheck: true
+    allowedHosts:
+      "2349cfab-389b-4af8-bb36-b56d10b9c576-00-2oqm0hxuwsgio.worf.replit.dev",
   },
   css: {
-    postcss: true
-  }
-})
+    postcss: true,
+  },
+});
