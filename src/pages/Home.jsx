13f06a4a46adc432aa7bb16cc578ffbf8155890b
vincent1229 <72140120+vincent1229@users.noreply.github.com>
2025-08-01 20:15:21 +0000

    tried chaning home tab

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,11 +10,6 @@ export default function Home() {
   return (
     <>
       <Hero />
-      <About />
-      <Projects />
-      <Skills />
-      <Certifications />
-      <Contact />
     </>
   );
 }
